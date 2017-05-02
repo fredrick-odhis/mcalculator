ebee28713666517466d638d7715b140f7de43b7e
Fredrick Odhiambo <odhiambofred12@gmail.com>
2017-05-02 12:29:41 +0300

    latest changes

--- mortgage_calculator.php
+++ mortgage_calculator.php
@@ -22,3 +22,55 @@ function addUIstyles() {
 }
 
 add_action('init', 'addUIstyles');
+
+function widget_display($atts){
+
+	$temp .= "
+    <div id='wid2'>
+    	<div>
+    		<b style='margin-left: -20px;'>Property Price:</b>
+    		<input id='user_input' type='text'>
+    	</div><br />
+    	<!-- Down payment slider !-->
+    	<div id='slider'>
+    		<b id='back'>Down Payment: </b>
+    		<label id='varry'></label>
+      		<div id='custom-handle' class='ui-slider-handle'></div>
+    	</div><br />
+    	<!-- interest slider !-->
+
+      <div>
+    		<div id='interest'>
+    			<b id='back'>Interest: </b>
+    			<label id='int_label'></label>
+      			<div id='custom-handle' class='ui-slider-handle'></div>
+    		</div>
+    	</div><br />
+
+      <!-- Years slider !-->
+    	<div>
+    		<div id='years'>
+    			<b id='back'>Years: </b>
+    			<label id='yrs_label'></label>
+      			<div id='custom-handle' class='ui-slider-handle'></div>
+    		</div>
+    	</div><br />
+    	<!-- Amount Monthly !-->
+    	<div>
+    		<b style='margin-left: -20px;'>Amount Monthly:</b>
+    		<label id='amount_monthly'>Monthly Amount Test</label>
+    	</div>
+
+    	<div>
+    		<input id='button' type='submit' value='Calculate'>
+    	</div>
+
+    </div>
+	";
+
+	echo $temp;
+
+}
+
+add_shortcode("mortgage_cal","widget_display");
+add_filter('widget_text','do_shortcode');
